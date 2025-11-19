9eb2eefa7ed403935ea8196fc54ffc0bbd008dc2
Efe <efecini@gmail.com>
2025-11-19 18:35:09 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0;
+    var rightDiceNumber = 5;
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button is tapped.")
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        leftDiceNumber = leftDiceNumber+1;
+        rightDiceNumber = rightDiceNumber-1;
+    }
+    
+
+}
+
